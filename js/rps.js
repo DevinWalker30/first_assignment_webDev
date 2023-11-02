96b5bd132b5700f9d976f79880d24f3b2d9b1762
DevinWalker30 <devinh2307@gmail.com>
2023-11-02 12:43:41 -0400

    commit

--- js/rps.js
+++ js/rps.js
@@ -7,18 +7,50 @@ const result = document.querySelector('.result')
 const userImg = document.querySelector('.user-img')
 const computerImg = document.querySelector('.comp-img')
 
-function rockBtnClick() {
-    result.innerText = 'clicked rock'
+const choices = ['rock', 'paper', 'scissors']
+let comp
+
+// function rockBtnClick() {
+//     result.innerText = 'clicked rock'
+// }
+
+// function paperBtnClick() {
+//     result.innerText = 'clicked paper'
+// }
+
+// function scissorsBtnClick() {
+//     result.innerText = 'clicked scissors'
+// }
+
+
+rockBtn.addEventListener('click', () => {
+    userImg.src = '../imgs/rps_imgs/' + rockBtn.id + '.png'
+    // result.style.color = 'red'
+    // If using a hyph. word then use caps background-color --> backgroundColor
+    compChoice()
+    result.innerText = comp
+    winLose(rockBtn.id)
+})
+paperBtn.addEventListener('click', () => {
+    userImg.src = '../imgs/rps_imgs/' + paperBtn.id + '.png'
+    compChoice()
+    result.innerText = comp
+    winLose(paperBtn.id)
+})
+scissorsBtn.addEventListener('click', () => {
+    userImg.src = '../imgs/rps_imgs/' + scissorsBtn.id + '.png'
+    compChoice()
+    result.innerText = comp
+    winLose(scissorsBtn.id)
+})
+
+
+function compChoice() {
+    const randNum = Math.floor(Math.random()*3)
+    comp = choices[randNum]
+    computerImg.src = '../imgs/rps_imgs/' + comp + '.png'
 }
 
-function paperBtnClick() {
-    result.innerText = 'clicked paper'
+function winLose(userChoice) {
+    console.log(comp, userChoice)
 }
-
-function scissorsBtnClick() {
-    result.innerText = 'clicked scissors'
-}
-
-rockBtn.addEventListener('click', rockBtnClick)
-paperBtn.addEventListener('click', paperBtnClick)
-scissorsBtn.addEventListener('click', scissorsBtnClick)
