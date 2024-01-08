5f7a535dcdeeb3e47bd57f3a71d25f608d22ec1d
DevinWalker30 <devinh2307@gmail.com>
2024-01-08 11:57:28 -0500

    commit

--- js/faq.js
+++ js/faq.js
@@ -1,25 +1,35 @@
-const btnOne = document.querySelector('.btn-one')
-const btnTwo = document.querySelector('.btn-two')
-const btnThree = document.querySelector('.btn-three')
-const btnFour = document.querySelector('.btn-four')
+const btns = document.querySelectorAll('.btn')
 
-const answerOne = document.querySelector('.answer one')
-const answerTwo = document.querySelector('.answer two')
-const answerThree = document.querySelector('.answer three')
-const answerFour = document.querySelector('.answer four')
+const answers = document.querySelectorAll('.answer')
 
-btnOne.addEventListener('click', () => {
-    whichOne(btnOne, answerOne)
-    // btnOne.src = '/imgs/icon-minus.svg'
+// btnOne.addEventListener('click', () => {
+//     whichOne(btnOne, answerOne)
+//     // btnOne.src = '/imgs/icon-minus.svg'
+// })
+
+// function whichOne(source, answer) {
+//     if (source.src == '/imgs/icon-plus.svg') {
+//         source.src = '/imgs/icon-minus.svg'
+//         answer.style.display = 'block'
+//     }
+//     else if (source.src == '/imgs/icon-minus.svg') {
+//         source.src = '/imgs/icon-plus.svg'
+//         answer.style.display = 'none'
+//     }
+// }
+
+btns.forEach(button => {
+    console.log(button.src)
+
+    button.addEventListener('click', () => {
+        let index = parseInt(button.id)
+        if (button.src == 'http://127.0.0.1:5500/imgs/icon-plus.svg') {
+            button.src = 'http://127.0.0.1:5500/imgs/icon-minus.svg'
+            answers[index].style.display = 'block'
+        }
+        else if (button.src == 'http://127.0.0.1:5500/imgs/icon-minus.svg') {
+            button.src = 'http://127.0.0.1:5500/imgs/icon-plus.svg'
+            answers[index].style.display = 'none'
+        }
+    })
 })
-
-function whichOne(source, answer) {
-    if (source.src == '/imgs/icon-plus.svg') {
-        source.src = '/imgs/icon-minus.svg'
-        answer.style.display = 'block'
-    }
-    else if (source.src == '/imgs/icon-minus.svg') {
-        source.src = '/imgs/icon-plus.svg'
-        answer.style.display = 'none'
-    }
-}
